c6fa2ffdb8d6060dcc4ff0e9402e081a90ec312f
Alex <aszhukov91@gmail.com>
2021-08-05 15:19:02 +0300

    update utils.py ( prefilter )

--- course_project/utils.py
+++ course_project/utils.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-def prefilter_items(data, take_n_popular=5000, item_features=None):
+def prefilter_items(data, take_n_popular=5000, min_price=0, max_price=40, item_features=None):
     # Уберем не интересные для рекоммендаций категории (department)
     if item_features is not None:
         department_size = pd.DataFrame(item_features. \
@@ -18,10 +18,10 @@ def prefilter_items(data, take_n_popular=5000, item_features=None):
 
     # Уберем слишком дешевые товары (на них не заработаем). 1 покупка из рассылок стоит 60 руб.
     data['price'] = data['sales_value'] / (np.maximum(data['quantity'], 1))
-    data = data[data['price'] > 2]
+    data = data[data['price'] > min_price] # 2
 
-    # Уберем слишком дорогие товарыs
-    data = data[data['price'] < 50]
+    # Уберем слишком дорогие товары
+    data = data[data['price'] < max_price] # 50
 
     # Возбмем топ по популярности
     popularity = data.groupby('item_id')['quantity'].sum().reset_index()
